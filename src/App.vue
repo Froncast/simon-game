292dbc8819bf5261706b7af477fd3c88cebb06fb
Oleg Tretyakov <ichimaru859@gmail.com>
2020-10-11 21:28:14 +0500

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Simon />
+  </div>
+</template>
+
+<script>
+import Simon from "@/components/Simon.vue";
+
+export default {
+  name: "App",
+  methods: {
+    voice() {
+      const audio = new Audio();
+      audio.src = require("@/assets/sounds/1.mp3");
+      audio.autoplay = true;
+    },
+  },
+  components: {
+    Simon,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
